7d375ca0b85c8ae75f63e48706c09ff5c955aab1
SuhailAhuja <99223756+SuhailAhuja@users.noreply.github.com>
2022-04-28 01:24:53 +0100

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -23,7 +23,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'GRsystem',
-    'GRsystem.apps.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
