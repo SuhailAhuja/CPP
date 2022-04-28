bd9af8273ed944bb02621f45bb9693ed2103220e
SuhailAhuja <99223756+SuhailAhuja@users.noreply.github.com>
2022-04-28 01:25:48 +0100

    Update apps.py

--- GRsystem/apps.py
+++ GRsystem/apps.py
@@ -1,8 +1,5 @@
 from django.apps import AppConfig
-from suit.apps import DjangoSuitConfig
 
-class SuitConfig(DjangoSuitConfig):
-    layout='vertical'
 class GrsystemConfig(AppConfig):
     name = 'GRsystem'
     
